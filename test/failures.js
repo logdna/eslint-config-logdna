d0180027aecfd8cefe7261ce6ad500f55730ad29
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-09-30 09:01:39 -0500

    feat!: drop max-len rule from 160 to 90
    This sets the maximum line length to 90 for lines of source code.
    Currently, comments are ignored so those lines are an exception to the
    rule.
    
    Semver: major
    Ref: LOG-7437

--- test/failures.js
+++ test/failures.js
@@ -5,16 +5,29 @@ const fs = require('fs')
 const {test, threw} = require('tap')
 const {CLIEngine} = require('eslint')
 const EOL_CODE = path.join(__dirname, 'fixtures', 'no-eol-fixture')
+const MAX_LEN_CODE = path.join(__dirname, 'fixtures', 'max-len-fixture')
 
 const readFile = fs.promises.readFile
 test('invalid config', async (t) => {
-  const code = await readFile(EOL_CODE, 'utf8')
   const cli = new CLIEngine({
     useEslintrc: false
   , configFile: 'eslintrc.json'
   })
 
-  const result = cli.executeOnText(code)
-  t.equal(result.errorCount, 1, 'new line missing')
-  t.equal(result.results[0].messages[0].ruleId, 'eol-last', 'missing newline')
+  t.test('no-eol', async (t) => {
+    const code = await readFile(EOL_CODE, 'utf8')
+    const result = cli.executeOnText(code)
+    t.equal(result.errorCount, 1, 'error count')
+    t.equal(result.results[0].messages[0].ruleId, 'eol-last', 'missing newline')
+  })
+
+  t.test('max-len', async (t) => {
+    const code = await readFile(MAX_LEN_CODE, 'utf8')
+    const result = cli.executeOnText(code)
+    t.equal(result.errorCount, 1, 'error count')
+    const messages = result.results[0].messages
+
+    t.equal(messages[0].ruleId, 'max-len', 'max length exceeded')
+    t.match(messages[0].message, /maximum allowed is 90/ig, 'message expected line length')
+  })
 }).catch(threw)
