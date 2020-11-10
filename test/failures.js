3eb2067a3f581eb78561a5f6933bf71920de28e4
Mike Del Tito <mike.deltito@logdna.com>
2020-11-10 01:14:10 -0500

    feat!: add eslint-plugin-logdna and associated config
    This adds eslint-plugin-logdna as a dependency, along with
    standard configuration for the provided rules.
    
    Semver: major

--- test/failures.js
+++ test/failures.js
@@ -6,12 +6,14 @@ const {test, threw} = require('tap')
 const {CLIEngine} = require('eslint')
 const EOL_CODE = path.join(__dirname, 'fixtures', 'no-eol-fixture')
 const MAX_LEN_CODE = path.join(__dirname, 'fixtures', 'max-len-fixture')
+const LOGDNA_PLUGIN_CODE = path.join(__dirname, 'fixtures', 'logdna-plugin-fixture')
 
 const readFile = fs.promises.readFile
 test('invalid config', async (t) => {
   const cli = new CLIEngine({
     useEslintrc: false
-  , configFile: 'eslintrc.json'
+  , cwd: __dirname
+  , configFile: '../eslintrc.json'
   })
 
   t.test('no-eol', async (t) => {
@@ -29,5 +31,40 @@ test('invalid config', async (t) => {
 
     t.equal(messages[0].ruleId, 'max-len', 'max length exceeded')
     t.match(messages[0].message, /maximum allowed is 90/ig, 'message expected line length')
+  })
+
+  t.test('plugin-logdna', async (t) => {
+    const code = await readFile(LOGDNA_PLUGIN_CODE, 'utf8')
+    const result = cli.executeOnText(code)
+    t.equal(result.errorCount, 4, 'error count')
+    const messages = result.results[0].messages
+
+    t.equal(messages[0].ruleId, 'logdna/require-file-extension', 'file extension required')
+    t.equal(
+      messages[0].message
+      , 'Missing file extension for local module.'
+      , 'message expected file extension'
+    )
+
+    t.equal(messages[1].ruleId, 'logdna/grouped-require', 'require grouping')
+    t.equal(
+      messages[1].message
+      , 'Expected \'builtin\' require before \'local\' require.'
+      , 'message expected for grouped require'
+    )
+
+    t.equal(messages[2].ruleId, 'logdna/tap-consistent-assertions', 'consistent assertions')
+    t.equal(
+      messages[2].message
+      , 'The "strictEqual" alias is preferred over "isEqual"'
+      , 'message expected preferred alias'
+    )
+
+    t.equal(messages[3].ruleId, 'logdna/tap-consistent-assertions', 'consistent assertions')
+    t.equal(
+      messages[3].message
+      , 'The "deepEqual" alias is preferred over "same"'
+      , 'message expected preferred alias'
+    )
   })
 }).catch(threw)
