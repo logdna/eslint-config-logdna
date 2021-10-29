d74acfe1a4e6cc18de27ffd375ce440c1c2fd06c
Eric Satterwhite <esatterwhite@wi.rr.com>
2021-10-29 17:27:30 -0500

    chore(deps)!: eslint-plugin-sensible@3.0.0
    update sensible pluging to include support for more ecmascript 2020
    features - optional chaining, class fields, etc.
    
    BREAKING CHANGES: Minimum eslint version 8.x
    BREAKING CHANGES: Mimimum node.js version 12.x

--- test/config.js
+++ test/config.js
@@ -4,17 +4,17 @@ const path = require('path')
 const fs = require('fs')
 const {promisify} = require('util')
 const {test, threw} = require('tap')
-const {CLIEngine} = require('eslint')
+const {ESLint} = require('eslint')
 const VALID_CODE = path.join(__dirname, 'fixtures', 'valid-code')
 
 const readFile = promisify(fs.readFile)
 test('valid config', async (t) => {
   const code = await readFile(VALID_CODE, 'utf8')
-  const cli = new CLIEngine({
+  const linter = new ESLint({
     useEslintrc: false
-  , configFile: '.eslintrc.json'
+  , overrideConfigFile: '.eslintrc.json'
   })
 
-  const result = cli.executeOnText(code)
+  const [result] = await linter.lintText(code)
   t.equal(result.errorCount, 0, 'error count')
 }).catch(threw)
--- test/failures.js
+++ test/failures.js
@@ -1,26 +1,26 @@
 'use strict'
 
 const path = require('path')
-const {CLIEngine} = require('eslint')
+const {ESLint} = require('eslint')
 const {test, threw} = require('tap')
 
 test('Invalid linting for larger code blocks read from fixtures', async (t) => {
-  const cli = new CLIEngine({
+  const linter = new ESLint({
     useEslintrc: false
   , cwd: path.join(__dirname, 'fixtures')
-  , configFile: '../../.eslintrc.json'
+  , overrideConfigFile: '../../.eslintrc.json'
   })
 
   t.test('no-eol', async (t) => {
-    const result = cli.executeOnFiles(['no-eol-fixture'])
+    const [result] = await linter.lintFiles(['no-eol-fixture'])
     t.equal(result.errorCount, 1, 'error count')
-    t.equal(result.results[0].messages[0].ruleId, 'eol-last', 'missing newline')
+    t.equal(result.messages[0].ruleId, 'eol-last', 'missing newline')
   })
 
   t.test('max-len', async (t) => {
-    const result = cli.executeOnFiles(['max-len-fixture'])
+    const [result] = await linter.lintFiles(['max-len-fixture'])
     t.equal(result.errorCount, 1, 'error count')
-    const messages = result.results[0].messages
+    const messages = result.messages
 
     t.equal(messages[0].ruleId, 'max-len', 'max length exceeded')
     t.match(
@@ -30,10 +30,10 @@ test('Invalid linting for larger code blocks read from fixtures', async (t) => {
     )
   })
 
-  t.test('no-multiple-empty-lines', async (t) => {
-    const result = cli.executeOnFiles(['no-multiple-empty-lines-fixture'])
+  t.test('no-multlintFilesiple-empty-lines', async (t) => {
+    const [result] = await linter.lintFiles(['no-multiple-empty-lines-fixture'])
     t.equal(result.errorCount, 1, 'error count')
-    const messages = result.results[0].messages
+    const messages = result.messages
 
     t.equal(messages[0].ruleId, 'no-multiple-empty-lines', 'multiple empty found')
     t.match(
@@ -44,14 +44,14 @@ test('Invalid linting for larger code blocks read from fixtures', async (t) => {
   })
 
   t.test('no-debugger', async (t) => {
-    const result = cli.executeOnFiles(['no-debugger-fixture'])
+    const [result] = await linter.lintFiles(['no-debugger-fixture'])
     t.equal(result.errorCount, 1, 'error count')
-    t.equal(result.results[0].messages[0].ruleId, 'no-debugger', 'missing newline')
+    t.equal(result.messages[0].ruleId, 'no-debugger', 'missing newline')
   })
 
-  t.test('plugin-logdna', async (t) => {
-    const result = cli.executeOnFiles(['logdna-plugin-fixture'])
-    const messages = result.results[0].messages
+  t.test('pluglintFilesin-logdna', async (t) => {
+    const [result] = await linter.lintFiles(['logdna-plugin-fixture'])
+    const messages = result.messages
     t.equal(result.errorCount, 6, 'error count')
 
     t.equal(
@@ -119,24 +119,26 @@ test('Invalid linting for larger code blocks read from fixtures', async (t) => {
 }).catch(threw)
 
 test('Invlalid linting with quick-and-dirty inline code', async (t) => {
-  const cli = new CLIEngine({
+  const linter = new ESLint({
     useEslintrc: false
   , cwd: __dirname
-  , configFile: '../.eslintrc.json'
-  , rules: {
-      'strict': 'off'
-    , 'sensible/indent': 'off'
-    , 'eol-last': 'off'
-    , 'no-unused-vars': 'off'
+  , overrideConfigFile: '../.eslintrc.json'
+  , overrideConfig: {
+      rules: {
+        'strict': 'off'
+      , 'sensible/indent': 'off'
+      , 'eol-last': 'off'
+      , 'no-unused-vars': 'off'
+      }
     }
   })
 
   t.test('arrow-parens', async (t) => {
     const code = '[].map(thing => { return thing + 1 })'
 
-    const result = cli.executeOnText(code)
+    const [result] = await linter.lintText(code)
     t.equal(result.errorCount, 1, 'error count')
-    const messages = result.results[0].messages
+    const messages = result.messages
 
     t.equal(messages[0].ruleId, 'arrow-parens', 'arrow-parens is the rule id')
     t.equal(
@@ -149,9 +151,9 @@ test('Invlalid linting with quick-and-dirty inline code', async (t) => {
   t.test('arrow-body-style', async (t) => {
     const code = '[].map((thing) => thing + 1)'
 
-    const result = cli.executeOnText(code)
+    const [result] = await linter.lintText(code)
     t.equal(result.errorCount, 1, 'error count')
-    const messages = result.results[0].messages
+    const messages = result.messages
 
     t.equal(
       messages[0].ruleId
@@ -168,9 +170,9 @@ test('Invlalid linting with quick-and-dirty inline code', async (t) => {
   t.test('func-style', async (t) => {
     const code = 'const NOOP = () => {}'
 
-    const result = cli.executeOnText(code)
+    const [result] = await linter.lintText(code)
     t.equal(result.errorCount, 1, 'error count')
-    const messages = result.results[0].messages
+    const messages = result.messages
 
     t.equal(messages[0].ruleId, 'func-style', 'func-style is the rule id')
     t.equal(
